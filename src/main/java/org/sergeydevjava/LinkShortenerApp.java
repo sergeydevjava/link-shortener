ea54d0ad2778326c2269f8cd0b8e485e7d1947cc
sergey <sergey.navrockiy@drsmile-group.com>
2024-10-04 11:51:39 +0700

    initial commit

---
+++ src/main/java/org/sergeydevjava/LinkShortenerApp.java
@@ -0,0 +1,11 @@
+package org.sergeydevjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkShortenerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(LinkShortenerApp.class, args);
+    }
+}
