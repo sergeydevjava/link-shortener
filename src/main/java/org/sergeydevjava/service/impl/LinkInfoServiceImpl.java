03b1baceef24b89dcb54860077ef17cae070667c
sergey <sergey.navrockiy@drsmile-group.com>
2024-10-08 08:52:53 +0700

    hw2 first review

--- src/main/java/org/sergeydevjava/service/impl/LinkInfoServiceImpl.java
+++ src/main/java/org/sergeydevjava/service/impl/LinkInfoServiceImpl.java
@@ -1,23 +1,24 @@
 package org.sergeydevjava.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.sergeydevjava.dto.CreateLinkInfoRequest;
 import org.sergeydevjava.service.LinkInfoService;
 import org.springframework.stereotype.Service;
-
-import java.util.function.BiFunction;
-import java.util.function.Supplier;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 @Service
 @RequiredArgsConstructor
 public class LinkInfoServiceImpl implements LinkInfoService {
 
-    private final Supplier<String> shortLinkGenerator;
-    private final BiFunction<String, CreateLinkInfoRequest, CreateLinkInfoRequest> repository;
+    public static final Integer LINK_LENGTH = 8;
+
+    ConcurrentMap<String, CreateLinkInfoRequest> repository = new ConcurrentHashMap<>();
 
     public String createShortLink(CreateLinkInfoRequest createLinkInfoRequest) {
-        String shortLink = shortLinkGenerator.get();
-        repository.apply(shortLink, createLinkInfoRequest);
+        String shortLink = RandomStringUtils.randomAlphanumeric(LINK_LENGTH);
+        repository.put(shortLink, createLinkInfoRequest);
         return shortLink;
     }
 }
