aa78b4973e910f5df7aa4895bbee6b63521f35a7
sergey <sergey.navrockiy@drsmile-group.com>
2024-10-27 21:56:14 +0700

    hw4

--- src/main/java/org/sergeydevjava/LinkShortenerApp.java
+++ src/main/java/org/sergeydevjava/LinkShortenerApp.java
@@ -1,7 +1,5 @@
 package org.sergeydevjava;
 
-
-
 import com.sergeydevjava.LoggingStarterAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
