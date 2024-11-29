8b0d97fb88e7e30fad408f154ec49c5ea9ac93df
sergey <sergey.navrockiy@drsmile-group.com>
2024-11-30 03:13:25 +0700

    hw11

--- src/main/java/org/sergeydevjava/config/LinkShortenerConfig.java
+++ src/main/java/org/sergeydevjava/config/LinkShortenerConfig.java
@@ -2,19 +2,21 @@ package org.sergeydevjava.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.ResourceUtils;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.FileCopyUtils;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class LinkShortenerConfig {
 
     @Bean
     public String notFoundPage() throws IOException {
-        File file = ResourceUtils.getFile("classpath:templates/404.html");
-        return Files.readString(Path.of(file.getPath()));
+        try (InputStream is = new ClassPathResource("templates/404.html").getInputStream()) {
+            byte[] binaryData = FileCopyUtils.copyToByteArray(is);
+            return new String(binaryData, StandardCharsets.UTF_8);
+        }
     }
 }
